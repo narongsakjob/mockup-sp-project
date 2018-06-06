afa44f05bac5e1b269fa8263387df37178fad162
Narongsak <narongsak.chobsri@gmail.com>
2018-06-06 14:17:23 +0700

    Add javascipt modules and set turbolinks

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,19 @@
-console.log("test")
+var ready = function() {
+
+  'use strict';
+
+  if (!window.AppMan) {
+    window.AppMan = {};
+  }
+
+  var AppMan = window.AppMan;
+  
+  AppMan.App = AppMan.App || {};
+  // Initialize application
+  AppMan.Init = (function() {
+    //Modules
+  })();
+
+
+};
+window.$(document).on("turbolinks:load", ready);
